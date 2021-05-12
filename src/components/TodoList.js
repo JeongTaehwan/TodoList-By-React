e4f115d133611f2420d10062da89c70cf239355b
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-05-12 21:05:21 +0900

    [ADD] TodoItem
    [ADD] TodoItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
     flex: 1;
     padding: 20px 32px;
     padding-bottom: 48px;
     overflow-y: auto;
-    background: gray;
 `;
 
 function TodoList() {
     return (
         <TodoListBlock>
-            투두리스트
+            <TodoItem text="프로젝트 생성하기" done={true} />
+            <TodoItem text="토익하기" done={true} />
+            <TodoItem text="API 공부하기" done={false} />
+            <TodoItem text="나는 GSM 4기" done={false} />
         </TodoListBlock>
     );
 }
